f28a85256e526d3a695364aa6abd9581c4c3a010
Arun Dhyani <dhyaniarun7@gmail.com>
2023-03-15 18:20:58 +0530

    generalised NFT minter added

--- contracts/tests/BasicNft.sol
+++ contracts/tests/BasicNft.sol
@@ -1,27 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract BasicNft is ERC721 {
-    string public constant TOKEN_URI ="ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+contract GeneralNft721 is ERC721URIStorage {
     uint256 private s_tokenCounter;
 
-    event DogMinted(uint256 indexed tokenId);
+    event NftMinted(uint256 indexed tokenId);
 
-    constructor() ERC721("Dogie", "DOG") {
+    constructor() ERC721("NFT Demo", "DEMO") {
         s_tokenCounter = 0;
     }
 
-    function mintNft() public {
+    function mintNFT(string memory _tokenURI) public {
         _safeMint(msg.sender, s_tokenCounter);
-        emit DogMinted(s_tokenCounter);
+        emit NftMinted(s_tokenCounter);
+        _setTokenURI(s_tokenCounter, _tokenURI);
         s_tokenCounter = s_tokenCounter + 1;
-    }
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return TOKEN_URI;
     }
 
     function getTokenCounter() public view returns (uint256) {
